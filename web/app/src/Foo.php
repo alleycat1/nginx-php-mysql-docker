9a13484d14f6129ab2d4498d3908f314ed5e2eb3
netragonlong <netragonlong@gmail.com>
2023-08-07 02:08:37 +0900

    Add project files

---
+++ web/app/src/Foo.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * I belong to a file
+ */
+
+namespace App\Acme;
+
+/**
+ * I belong to a class
+ */
+class Foo
+{
+    /**
+     * Gets the name of the application.
+     */
+    public function getName()
+    {
+        return 'Nginx PHP MySQL';
+    }
+}
